6c762b4adcc75333890000ecf35cf284c338fbca
Hugo Wainwright <hugowainwright@fb.com>
2019-04-11 13:48:51 +0100

    improved local deployment script

--- pypi/main.py
+++ pypi/main.py
@@ -1,8 +1,17 @@
-from leaderboardweb import app
+from leaderboardweb import app as leaderboard_web_app
+from leaderboarddata import app as leaderboard_data_app
+import werkzeug.serving
+import werkzeug.wrappers
 
 
 def main():
-    app.run(host='127.0.0.1', port=32387, debug=True)
+    app = werkzeug.wsgi.DispatcherMiddleware(
+        werkzeug.wrappers.Response("Nothing to see here!", status=404),
+        mounts={"/leaderboard": leaderboard_web_app,
+                "/leaderboard_data": leaderboard_data_app}
+    )
+    werkzeug.serving.run_simple(
+        "127.0.0.1", 32387, app, use_reloader=True, use_debugger=True, threaded=True)
 
 
 if __name__ == "__main__":
